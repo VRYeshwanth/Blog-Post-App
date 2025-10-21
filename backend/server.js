0387cf8fe9d290e0a2993ffcd77f66e7d3847387
Yeshwanth <vryeshwanth06@gmail.com>
2025-10-21 10:48:28 +0530

    Setup Server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("✅ MongoDB Connected Successfully"))
+.catch(console.log("❌ Error connecting to database"));
 
 app.listen(process.env.PORT, () => {
     console.log(`✅ Server running on PORT ${process.env.PORT}`);
