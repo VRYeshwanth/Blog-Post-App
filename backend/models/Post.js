d8c8a0648626f5b84496ed5fe86752bdf1725f33
Yeshwanth <vryeshwanth06@gmail.com>
2025-10-21 11:17:56 +0530

    Implement user and post models

---
+++ backend/models/Post.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: Date
+})
+
+postSchema.pre("save", function(next) {
+    this.updatedAt = new Date();
+    next();
+})
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
