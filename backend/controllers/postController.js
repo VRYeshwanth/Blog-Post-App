ea1096360c31016a2bddadbfe26e103300c876ce
Yeshwanth <vryeshwanth06@gmail.com>
2025-10-22 11:51:20 +0530

    Setup PATCH route

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -20,3 +20,25 @@ export const createPost = async (req, res) => {
         res.status(400).json({"Error": err.message});
     }
 }
+
+export const updatePost = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, content } = req.body;
+
+        const existingPost = await Post.findOne({_id: id});
+
+        if(!existingPost) return res.status(400).json({"error": "Post not found!!"})
+
+        if(existingPost.author.toString() !== req.userId) return res.status(400).json({"error": "Access Denied !!"});
+
+        existingPost.title = title;
+        existingPost.content = content;
+
+        await existingPost.save();
+
+        res.status(201).json(existingPost);
+    } catch (err) {
+        res.status(500).json({"error": err.message});
+    }
+}
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getPosts, createPost } from "../controllers/postController.js";
+import { getPosts, createPost, updatePost } from "../controllers/postController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
 router.post("/", verifyToken, createPost);
+router.patch("/:id", verifyToken, updatePost);
 
 export default router;
