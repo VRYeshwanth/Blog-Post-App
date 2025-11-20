38ba12d4a53d79ea686d8d12bcec95d32b26c679
Yeshwanth <vryeshwanth06@gmail.com>
2025-11-20 17:19:38 +0530

    add and style edit and delete buttons

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,11 +1,20 @@
+import { useAuth } from "../../context/AuthContext";
 import { formatTime } from "../../utils/FormatTime";
 export default function Comment({ comment }) {
+    const { auth } = useAuth();
+
     return (
         <div className="comment">
             <div className="header">
                 <div className="username">
                     <h3>{comment.userId.username}</h3>
                 </div>
+                {auth.userId === comment.userId._id && (
+                    <div className="header-btns">
+                        <i className="bx bx-edit"></i>
+                        <i className="bx bx-trash"></i>
+                    </div>
+                )}
             </div>
             <div className="body">
                 <p>{comment.text}</p>
