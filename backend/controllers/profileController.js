456f6418913d3d9712a6ba2cedcbaceaec5682b2
Yeshwanth <vryeshwanth06@gmail.com>
2026-01-05 17:33:52 +0530

    Add function to delete user account

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,4 +1,6 @@
 import User from "../models/User.js";
+import Post from "../models/Post.js";
+import Comment from "../models/Comment.js";
 
 export const getProfileDetails = async (req, res) => {
     try {
@@ -43,6 +45,27 @@ export const updateProfileDetails = async (req, res) => {
                 email: user.email,
             },
         });
+    } catch (err) {
+        return res.status(500).json({ error: err.message });
+    }
+};
+
+export const deleteAccount = async (req, res) => {
+    try {
+        const userId = req.user.id;
+
+        const user = await User.findById(userId);
+        if (!user)
+            return res.status(404).json({ message: "User not found !!" });
+
+        await Post.updateMany({ likes: userId }, { $pull: { likes: userId } });
+        await Comment.deleteMany({ userId: userId });
+        await Post.deleteMany({ author: userId });
+        await User.deleteOne({ _id: userId });
+
+        return res
+            .status(200)
+            .json({ message: "Account successfully deleted" });
     } catch (err) {
         return res.status(500).json({ error: err.message });
     }
--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
     getProfileDetails,
     updateProfileDetails,
+    deleteAccount,
 } from "../controllers/profileController.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.get("/", verifyToken, getProfileDetails);
 router.patch("/", verifyToken, updateProfileDetails);
+router.delete("/", verifyToken, deleteAccount);
 
 export default router;
